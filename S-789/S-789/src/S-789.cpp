3f770f70051009e203f674130d87dec70d9243ed
Parubets Evgeny <parubets.e@alager.ru>
2020-08-08 13:51:50 +0300

    dallas  работает

--- S-789/S-789/src/S-789.cpp
+++ S-789/S-789/src/S-789.cpp
@@ -1,12 +1,12 @@
 #include "TM1637.h"
 #include <OneWire.h>
 #include <LiquidCrystal.h>
-//#include <DallasTemperature.h>
+#include <DallasTemperature.h>
 
 #define ONE_WIRE_BUS 11
 OneWire ds1820(ONE_WIRE_BUS);// Создаем объект OneWire для шины 1-Wire, с помощью которого будет осуществляться работа с датчиком
 // Pass our oneWire reference to Dallas Temperature.
-//DallasTemperature sensors(&ds1820);
+DallasTemperature sensor(&ds1820);
 
 #define RS 9 //pins definitions for 1602
 #define E 8
@@ -37,7 +37,33 @@ bool state_relay_mikrocomputer_1=true;
 bool state_relay_mikrocomputer_2=true;
 bool state_relay_heater_cable=true;
 //int8_t DispMSG[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-float tempSensor ;
+float tempSensor =0;
+int temp_minus;
+byte data[9]{B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B11111100,B10010000}; // это -55 градусов
+
+DeviceAddress insideThermometer, outsideThermometer; // arrays to hold device addresses
+
+
+ void receive_temp_dallas(DeviceAddress deviceAddress){
+     sensor.begin();
+     sensor.getAddress(insideThermometer, 0);
+     sensor.setHighAlarmTemp(insideThermometer, 50);
+     sensor.setLowAlarmTemp(insideThermometer, -20);
+     sensor.requestTemperatures();
+//     tempSensor = sensor.getTempCByIndex(0);
+     tempSensor = sensor.getTempC(deviceAddress);
+
+}
+
+ void checkAlarm(DeviceAddress deviceAddress)
+  {
+    if (sensor.hasAlarm(deviceAddress))
+    {
+        lcd.setCursor(0, 0);
+        lcd.print("Alarm");
+    }
+
+  }
 
  void receive_temp_no_delay(){
 //#include <OneWire.h>
@@ -73,35 +99,32 @@ float tempSensor ;
 
  }
 
- int temp_minus=0;
-// byte data[2] = {B11111100,B10010000}; // это -55 Место для значения температуры
- byte data[9]{B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B11111100,B10010000};
-
  void receive_temperature(){
-     // Определяем температуру от датчика DS18b20
-     ds1820.reset(); // Начинаем взаимодействие со сброса всех предыдущих команд и параметров
-     ds1820.write(0xCC); // Даем датчику DS18b20 команду пропустить поиск по адресу. В нашем случае только одно устрйоство
-     ds1820.write(0x44); // Даем датчику DS18b20 команду измерить температуру. Само значение температуры мы еще не получаем - датчик его положит во внутреннюю память
-     delay(1000); // Микросхема измеряет температуру, а мы ждем.
-     ds1820.reset(); // Теперь готовимся получить значение измеренной температуры
-     ds1820.write(0xCC);
-     ds1820.write(0xBE); // Просим передать нам значение регистров со значением температуры
-     // Получаем и считываем ответ
-     data[8] = ds1820.read(); // Читаем младший байт значения температуры
-     data[7] = ds1820.read(); // старший
-     // Формируем итоговое : "склеиваем" значение,затем умножаем его на коэффициент, соответсвующий разрешающей способности (для 12 бит по умолчанию - это 0,0625)
-     temp_minus = 0;
-     temp_minus = data[7] & B10000000;
-     if (temp_minus != 128){
-     tempSensor =  ((data[7] << 8) | data[8]) * 0.0625;
-     }
-     if (temp_minus == 128){
-         tempSensor =  ((~((data[7] << 8) | data[8]))+1) * 0.0625;
-     }
+         // Определяем температуру от датчика DS18b20
+         ds1820.reset(); // Начинаем взаимодействие со сброса всех предыдущих команд и параметров
+         ds1820.write(0xCC); // Даем датчику DS18b20 команду пропустить поиск по адресу. В нашем случае только одно устрйоство
+         ds1820.write(0x44); // Даем датчику DS18b20 команду измерить температуру. Само значение температуры мы еще не получаем - датчик его положит во внутреннюю память
+         delay(1000); // Микросхема измеряет температуру, а мы ждем.
+         ds1820.reset(); // Теперь готовимся получить значение измеренной температуры
+         ds1820.write(0xCC);
+         ds1820.write(0xBE); // Просим передать нам значение регистров со значением температуры
 
+         // Получаем и считываем ответ
+         data[8] = ds1820.read(); // Читаем младший байт значения температуры
+         data[7] = ds1820.read(); // Читаем старший байт значения температуры
+
+         // Формируем температуру для вывода: "склеиваем" значения,затем умножаем на коэффициент, (для 12 бит по умолчанию - это 0,0625)
+         temp_minus = 0;
+         temp_minus = data[7] & B10000000;
+         if (temp_minus != 128){
+            tempSensor =  ((data[7] << 8) | data[8]) * 0.0625;
+         }
+         if (temp_minus == 128){
+             tempSensor =  ((~((data[7] << 8) | data[8]))+1) * 0.0625;
+         }
   }
 
- void print_temperature() {
+ void print_temperature_1637() {
       int8_t Digits[] = {0x00, 0x00, 0x00, 0x00};
       int KL1 = tempSensor;
       int KL2 = (tempSensor - KL1) * 100;
@@ -109,29 +132,37 @@ float tempSensor ;
       //tm1637.point(POINT_ON);
       Digits[0] = (KL1 / 10); // раскидываем 4-значное число на цифры
       Digits[1] = (KL1 % 10);
-//      Digits[2] = (KL2 / 10);
-//      Digits[3] = (KL2 % 10);
-      tm1637.display(2,Digits[0]);
-      tm1637.display(3,Digits[1]);
+      Digits[2] = (KL2 / 10);
+      Digits[3] = (KL2 % 10);
+
+      tm1637.display(0,Digits[0]);
+      tm1637.point(1);
+      tm1637.display(1,Digits[1]);
+      tm1637.display(2,Digits[2]);
+      tm1637.display(3,Digits[3]);
    }
 
- void print_lcd_setup(){
-     lcd.begin(16, 2);                  // Задаем размерность экрана
-//     lcd.setCursor(0, 0);              // Устанавливаем курсор в начало 1 строки
-//     lcd.print("Temperature:");       // Выводим текст
-//     lcd.setCursor(0, 1);              // Устанавливаем курсор в начало 2 строки
-
-  }
+ void print_temperature_1602() {
+       lcd.setCursor(0, 1);
+       if(temp_minus != 128) lcd.print("+");
+       if(temp_minus == 128) lcd.print("-");
+       lcd.print(tempSensor);
+       //    lcd.setCursor(0, 0);
+       //    lcd.print(data[0]);
+       //    lcd.setCursor(0, 1);
+       //    lcd.print(data[1]);
+   }
 
  void setup()
   {
-  print_lcd_setup();
+   tm1637.init();
+   tm1637.set(BRIGHT_DARKEST);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+   lcd.begin(16, 2);                  // Задаем размерность экрана
+   //     lcd.setCursor(0, 0);              // Устанавливаем курсор в начало 1 строки
+   //     lcd.print("Temperature:");       // Выводим текст
+   //     lcd.setCursor(0, 1);              // Устанавливаем курсор в начало 2 строки
 
 
-    tm1637.init();
-    tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
-
-  //    sensors.begin();
 
    pinMode(LED_BUILTIN, OUTPUT);// initialize digital pin LED_BUILTIN as an output.
    pinMode(ping1, INPUT);
@@ -139,25 +170,17 @@ float tempSensor ;
    pinMode(relay_mikrocomputer_1, OUTPUT);
 //   pinMode(relay_mikrocomputer_2, OUTPUT);
 //   pinMode(relay_heater_cable, OUTPUT);
-   tm1637.init();
-   tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+
   }
-
-
 
  void loop()
  {
-  receive_temperature();
-  print_temperature();
-
-  lcd.setCursor(0, 1);
-  if(temp_minus != 128) lcd.print("+");
-  if(temp_minus == 128) lcd.print("-");
-  lcd.print(tempSensor);
-//    lcd.setCursor(0, 0);
-//    lcd.print(data[0]);
-//    lcd.setCursor(0, 1);
-//    lcd.print(data[1]);
+  receive_temp_dallas(insideThermometer);
+//  receive_temperature();
+  print_temperature_1637();
+  print_temperature_1602();
+//  checkAlarm(insideThermometer);
+//  sensor.resetAlarmSearch();
 
   ping1State = digitalRead(ping1);
   if (ping1State_read != ping1State) {
