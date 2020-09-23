97b431ee1931ea82dd6f421569042c3a0cf6ba36
8evgeny <8evgeny@gmail.com>
2020-09-23 11:33:15 +0300

    Включил пинг

--- ARDUINO_PROJECTS/S-789/src/S-789.cpp
+++ ARDUINO_PROJECTS/S-789/src/S-789.cpp
@@ -306,44 +306,44 @@
 
 //Откдючена проверка пинга до окончания сертификации
 
-//  ping_status = checkPing();
-//  if(!ping_status){ //действия если нет пинга
-//   lcd.setCursor(7, 1);
-//   lcd.print("NoPing  ");
-//   tempSensor = receive_temp();
-//   print_temperature_1602(tempSensor);
-//   print_temperature_1637(tempSensor);
-//   powerBoard1(0);
-//   delay(5000);
-//   ++restart;
-//   if(number_restart == restart-1){
-//      lcd.setCursor(7, 1);
-//      lcd.print("wait ....");
-//      lcd.print(restart);
-//   delay(WAIT_PING_RESTART);
+  ping_status = checkPing();
+  if(!ping_status){ //действия если нет пинга
+   lcd.setCursor(7, 1);
+   lcd.print("NoPing  ");
+   tempSensor = receive_temp();
+   print_temperature_1602(tempSensor);
+   print_temperature_1637(tempSensor);
+   powerBoard1(0);
+   delay(5000);
+   ++restart;
+   if(number_restart == restart-1){
+      lcd.setCursor(7, 1);
+      lcd.print("wait ....");
+      lcd.print(restart);
+   delay(WAIT_PING_RESTART);
 
-//  restart = 0;
-//  }
-//   lcd.setCursor(7, 1);
-//   lcd.print("Restart");
-//   lcd.print(restart);
+  restart = 0;
+  }
+   lcd.setCursor(7, 1);
+   lcd.print("Restart");
+   lcd.print(restart);
 
-//  powerBoard1(1);
-//  delay(WAIT_POWER_ON);//ждем прогрузки платы
-//  }
+  powerBoard1(1);
+  delay(WAIT_POWER_ON);//ждем прогрузки платы
+  }
 
-//  if(ping_status && (restart!=0)){ //пинг появился
-//  restart = 0;
-//  lcd.setCursor(7, 1);
-//  lcd.print("        ");
-//  }
+  if(ping_status && (restart!=0)){ //пинг появился
+  restart = 0;
+  lcd.setCursor(7, 1);
+  lcd.print("        ");
+  }
 
-//  if(ping_status ){ //пинг ok
-//  lcd.setCursor(7, 1);
-//  lcd.print("PingOK  ");
-//  }
+  if(ping_status ){ //пинг ok
+  lcd.setCursor(7, 1);
+  lcd.print("PingOK  ");
+  }
 
-// digitalWrite(LED_BUILTIN, ping1_A); //светодиод на ардуине моргает по пингу
+ digitalWrite(LED_BUILTIN, ping1_A); //светодиод на ардуине моргает по пингу
 
 }
 
