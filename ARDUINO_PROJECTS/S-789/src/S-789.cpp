53826fe9d5153c0e925642f20f6dd8fba9de7a57
8evgeny <8evgeny@gmail.com>
2020-09-30 12:59:22 +0300

    отключение принудительного подогрева

--- ARDUINO_PROJECTS/S-789/src/S-789.cpp
+++ ARDUINO_PROJECTS/S-789/src/S-789.cpp
@@ -238,12 +238,12 @@ return pingstate;
 void powerCable(bool action){
 
 // Принудительно включаем подогрев
- digitalWrite(relay_heater_cable, 1);
- state_relay_heater_cable = 1;
+// digitalWrite(relay_heater_cable, 1);
+// state_relay_heater_cable = 1;
 
+ digitalWrite(relay_heater_cable, action);
+ state_relay_heater_cable = action;
 
-// digitalWrite(relay_heater_cable, action);
-// state_relay_heater_cable = action;
  lcd.setCursor(0, 0);
  lcd1.setCursor(0, 0);
  if(action){
