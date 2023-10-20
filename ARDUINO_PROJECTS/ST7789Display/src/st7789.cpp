2c1af8620510209fcb4539397e9b55ea8904167d
8evgeny <8evgeny@gmail.com>
2023-10-20 19:09:23 +0300

    add DUDE GUI

--- ARDUINO_PROJECTS/ST7789Display/src/st7789.cpp
+++ ARDUINO_PROJECTS/ST7789Display/src/st7789.cpp
@@ -13,67 +13,70 @@ void setup(void)
   // if the display has CS pin try with SPI_MODE0
   tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixel
   // if the screen is flipped, remove this command
-  tft.setRotation(2);
-  Serial.println(F("Initialized"));
-  uint16_t time = millis();
-  tft.fillScreen(ST77XX_BLACK);
-  time = millis() - time;
-  delay(500);
+//  tft.setRotation(2);
+//  Serial.println(F("Initialized"));
+//  uint16_t time = millis();
+//  tft.fillScreen(ST77XX_BLACK);
+//  time = millis() - time;
+//  delay(500);
 
-  // large block of text
-  tft.fillScreen(ST77XX_BLACK);
-  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
-  delay(1000);
+//  // large block of text
+//  tft.fillScreen(ST77XX_BLACK);
+//  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+//  delay(1000);
 
-  // tft print function!
-  tftPrintTest();
-  delay(4000);
+//  // tft print function!
+//  tftPrintTest();
+//  delay(4000);
 
-  // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
-  delay(500);
+//  // a single pixel
+//  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
+//  delay(500);
 
-  // line draw test
-  testlines(ST77XX_YELLOW);
-  delay(500);
+//  // line draw test
+//  testlines(ST77XX_YELLOW);
+//  delay(500);
 
-  // optimized lines
-  testfastlines(ST77XX_RED, ST77XX_BLUE);
-  delay(500);
+//  // optimized lines
+//  testfastlines(ST77XX_RED, ST77XX_BLUE);
+//  delay(500);
 
-  testdrawrects(ST77XX_GREEN);
-  delay(500);
+//  testdrawrects(ST77XX_GREEN);
+//  delay(500);
 
-  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
-  delay(500);
+//  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
+//  delay(500);
 
-  tft.fillScreen(ST77XX_BLACK);
-  testfillcircles(10, ST77XX_BLUE);
-  testdrawcircles(10, ST77XX_WHITE);
-  delay(500);
+//  tft.fillScreen(ST77XX_BLACK);
+//  testfillcircles(10, ST77XX_BLUE);
+//  testdrawcircles(10, ST77XX_WHITE);
+//  delay(500);
 
-  testroundrects();
-  delay(500);
+//  testroundrects();
+//  delay(500);
 
-  testtriangles();
-  delay(500);
+//  testtriangles();
+//  delay(500);
 
-  mediabuttons();
-  delay(500);
+//  mediabuttons();
+//  delay(500);
 
-  Serial.println("done");
-  delay(1000);
+//  Serial.println("done");
+//  delay(1000);
 }
 
 void loop() {
-//  tft.invertDisplay(true);
-//  delay(500);
-//  tft.invertDisplay(false);
-//  delay(500);
+  tft.invertDisplay(true);
+  delay(500);
+  tft.invertDisplay(false);
+  delay(500);
   testtriangles();
   delay(2000);
-  tftPrintTest();
-  delay(2000);
+
+//      testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
+    //  delay(500);
+//  tftPrintTest();
+//  delay(2000);
 }
 
 void testlines(uint16_t color) {
