667a3a92376bf36e5a7a22114af41ba7f4d6add0
8evgeny <8evgeny@gmail.com>
2023-11-11 23:15:23 +0300

    add

---
+++ ARDUINO_PROJECTS/sketch_oct22a/sketch_oct22a.ino
@@ -0,0 +1,16 @@
+int pin = 2;
+
+void setup() {
+  // Переключаем контакт GPIO2 в режим вывода данных (OUTPUT):
+  pinMode(pin, OUTPUT);
+}
+
+// Код в блоке loop() будет повторяться вновь и вновь:
+void loop() {
+  digitalWrite(pin, HIGH);   
+                             
+  delay(1000);               
+  digitalWrite(pin, LOW);    
+                             
+  delay(50);               
+}
